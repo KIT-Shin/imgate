ce856ebfe0bda64cf993d53e661e9f3e87377a6e
森部　想水 <sousuimoribe@moribesousuinoMacBook-Pro.local>
2019-09-06 22:30:47 +0900

    imgate_login.htmlにform要素を使ってユーザ名と画像ファイルを選択する場所を作成しました

--- webapp/imgate_webapp/imgate/forms.py
+++ webapp/imgate_webapp/imgate/forms.py
@@ -1,6 +1,11 @@
 from django import forms
 from .models import Imgate
 
+from django.contrib.auth.forms import (
+    AuthenticationForm
+)
+
+
 class ImgateForm(forms.ModelForm):
     """
     新規データ登録画面用のフォーム定義
@@ -8,3 +13,13 @@ class ImgateForm(forms.ModelForm):
     class Meta:
         model = Imgate
         fields = ['username', 'image']
+
+
+class LoginForm(AuthenticationForm):
+    """ログインフォーム"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['placeholder'] = field.label #placeholderにフィールドのラベルを入れる
--- webapp/imgate_webapp/imgate/views.py
+++ webapp/imgate_webapp/imgate/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import (
+    LoginView, LogoutView
+)
+from django.views import generic
+from .forms import LoginForm
+
 # Create your views here.
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from .models import Imgate
@@ -37,3 +44,16 @@ def render_login(request):
 
 def render_mypage(request):
     return render(request,"imgate/imgate_mypage.html")
+
+
+class Top(generic.TemplateView):
+    template_name = 'imgate/top.html'
+
+class Login(LoginView):
+    """ログインページ"""
+    form_class = LoginForm
+    template_name = 'imgate/login.html'
+
+class Logout(LoginRequiredMixin, LogoutView):
+    """ログアウトページ"""
+    template_name ='imgate/top.html'
